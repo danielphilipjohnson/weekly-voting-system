2fbb0b8a8417fcff71d1f3f3b859bccd067c9f49
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-18 19:12:50 +0100

    Models added * Employee model with fields of name and nickname * Winner model with fields of employee a FK and a score

--- hiyield/voting/models.py
+++ hiyield/voting/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    nickname = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Winner(models.Model):
+    employee = models.ForeignKey(
+        Employee, related_name='employee_winner', on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.employee
