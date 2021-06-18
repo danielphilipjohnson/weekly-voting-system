989085055400e9078ff2155baef9121dc1f825c2
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-18 19:47:02 +0100

    added post to add some users!

--- hiyield/voting/views.py
+++ hiyield/voting/views.py
@@ -11,6 +11,23 @@ class EmployeeList(APIView):
         serializer = EmployeeSerializer(employees, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request, *args, **kwargs):
+            data = {
+                'name': request.data.get('name'), 
+                'nickname': request.data.get('nickname'), 
+            }
+
+            employeeSerializer = EmployeeSerializer(data=data)
+            
+            if employeeSerializer.is_valid():
+                user = employeeSerializer.save()
+                winner = Winner(employee=user, score=0)
+                winner.save()
+             
+                return Response(employeeSerializer.data, status=status.HTTP_201_CREATED)
+
+            return Response(employeeSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class WinnersList(APIView):
     def get(self, request, *args, **kwargs):
         '''
