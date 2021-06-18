5bdfbe28e78391880f185cb1fa944da50f3a516f
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-18 20:40:25 +0100

    created footer

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -2,13 +2,16 @@
   <nav-bar />
 
   <router-view />
+  <Footer />
 </template>
 <script>
 import NavBar from "@/layout/Navbar";
+import Footer from "@/layout/Footer";
 
 export default {
   components: {
     NavBar,
+    Footer,
   },
 };
 </script>
