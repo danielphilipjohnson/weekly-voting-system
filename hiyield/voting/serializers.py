f1f0d17e674c53b8b3b47e0775b2f70d2ccaf86a
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-18 19:37:57 +0100

    hotfix: corrected spelling mistake in file

---
+++ hiyield/voting/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Employee, Winner
+
+class EmployeeSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Employee 
+    fields = '__all__'
+    
+# class WinnerSerializer(serializers.ModelSerializer):
+#   employee = EmployeeSerializer(many=False)
+#   class Meta:
+#     model = Winner
+#     ordering = ['score']
+#     fields =  '__all__'
