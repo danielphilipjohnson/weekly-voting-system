d4df3231d1ee314cd29aa08d2a57be6c108dd024
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-18 21:18:57 +0100

    added basic state

--- vue-frontend/src/store/index.js
+++ vue-frontend/src/store/index.js
@@ -1,8 +1,17 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    employees: null,
+  },
+  getters: {
+    employees: (state) => state.employees,
+  },
+  mutations: {
+    SET_EMPLOYEES(state, employees) {
+      state.employees = employees;
+    },
+  },
   actions: {},
   modules: {},
 });
