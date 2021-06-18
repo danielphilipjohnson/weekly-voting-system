5ce671f5d9b6f64a9993c75264770355128b9e37
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-18 19:20:01 +0100

    created EmployeeList view!

--- hiyield/voting/views.py
+++ hiyield/voting/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import EmployeeSerializer
+from voting.models import Employee
+
+class EmployeeList(APIView):
+    def get(self, request, *args, **kwargs):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    
