2ff2b2281df1f5ad0b7f8334cfddecdc12240237
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-18 22:05:45 +0100

    winners displayed in ascending order

--- vue-frontend/src/views/Home.vue
+++ vue-frontend/src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <section class="container mx-auto">
     <h1 class="text-center text-5xl font-bold py-8">Voting</h1>
-
     <section class="flex flex-shrink-0 flex-wrap justify-between">
-      <employee-card />
-      <employee-card />
-      <employee-card />
+      <div
+        class="mx-6 px-8 py-4 shadow-xl border mb-8 w-64"
+        v-for="fetchedEmployee in fetchedEmployees"
+        :key="fetchedEmployee.id"
+      >
+        <employee-card
+          v-bind:employee="fetchedEmployee.employee"
+          v-bind:id="fetchedEmployees.id"
+          v-bind:score="fetchedEmployee.score"
+        />
+      </div>
     </section>
   </section>
 </template>
@@ -16,6 +23,11 @@ export default {
   name: "Home",
   components: {
     EmployeeCard,
+  },
+  computed: {
+    fetchedEmployees() {
+      return this.$store.getters.employees;
+    },
   },
   mounted() {
     this.$store.dispatch("getEmployees");
